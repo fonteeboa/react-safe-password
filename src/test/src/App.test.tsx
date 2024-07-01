f38d29cf1a3bf4f5ef15ea9612da3fc698bc3162
João Victor <galvao845@gmail.com>
2024-07-01 19:47:31 -0300

    jest: adicionando testes unitario
    adicionando mais testes unitarios almejando a cobertura total dos testes

--- src/test/src/App.test.tsx
+++ src/test/src/App.test.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from '../../pages/App';
+import { useTranslation } from 'react-i18next';
+import { message } from 'antd';
+
+jest.mock('antd', () => ({
+  ...jest.requireActual('antd'),
+  message: {
+    info: jest.fn(),
+    error: jest.fn(),
+    success: jest.fn(),
+  },
+}));
+
+const setup = () => {
+  render(<App />);
+  const generateButton = screen.getByText(/Generate Password/i) as HTMLInputElement;
+  const lengthInput = screen.getByPlaceholderText('Password Length (12-32)') as HTMLInputElement;
+  const passwordInput = screen.getByPlaceholderText(/Generated Password/i) as HTMLInputElement;
+  const userWordInput = screen.getByPlaceholderText('Optional word') as HTMLInputElement;
+  const userWordOutput = screen.getByPlaceholderText('Generated Password');
+  const copyButton = screen.getByText(/Copy Password/i) as HTMLInputElement;
+  return { generateButton, lengthInput, passwordInput, userWordInput, userWordOutput, copyButton };
+};
+
 
 test('renders Secure Password Generator title', () => {
   render(<App />);
@@ -9,10 +32,7 @@ test('renders Secure Password Generator title', () => {
 });
 
 test('toggles switches and generates password', () => {
-  render(<App />);
-
-  const generateButton = screen.getByText(/Generate Password/i);
-  const passwordInput = screen.getByPlaceholderText(/Generated Password/i) as HTMLInputElement;
+  const { generateButton, passwordInput } = setup();
 
   // Ensure password input is initially empty
   expect(passwordInput).toHaveValue('');
@@ -42,11 +62,7 @@ test('toggles switches and generates password', () => {
 });
 
 test('copies password to clipboard', () => {
-  render(<App />);
-
-  const generateButton = screen.getByText(/Generate Password/i);
-  const copyButton = screen.getByText(/Copy Password/i);
-  const passwordInput = screen.getByPlaceholderText(/Generated Password/i) as HTMLInputElement;
+  const { generateButton, passwordInput, copyButton } = setup();
 
   // Mock clipboard API
   const writeText = jest.fn();
@@ -61,4 +77,96 @@ test('copies password to clipboard', () => {
 
   // Ensure clipboard writeText method was called with the generated password
   expect(writeText).toHaveBeenCalledWith(passwordInput.value);
+});
+
+test('adjusts length if it is less than 12 or greater than 32', () => {
+  const { generateButton, lengthInput } = setup();
+
+  // Set length to an invalid value (less than 12)
+  fireEvent.change(lengthInput, { target: { value: '10' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('Invalid length value. Generating password with length between 12 and 32 characters.');
+  expect(Number(lengthInput.getAttribute('value'))).toBeGreaterThanOrEqual(12);
+  expect(Number(lengthInput.getAttribute('value'))).toBeLessThanOrEqual(32);
+
+  // Set length to an invalid value (greater than 32)
+  fireEvent.change(lengthInput, { target: { value: '33' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('Invalid length value. Generating password with length between 12 and 32 characters.');
+  expect(Number(lengthInput.getAttribute('value'))).toBeGreaterThanOrEqual(12);
+  expect(Number(lengthInput.getAttribute('value'))).toBeLessThanOrEqual(32);
+});
+
+test('trims user word if its length is greater than or equal to the adjusted length', () => {
+  const { generateButton, lengthInput, userWordInput, userWordOutput } = setup();
+
+  // Set a valid length
+  fireEvent.change(lengthInput, { target: { value: '16' } });
+
+  // Set user word to a length greater than the length
+  fireEvent.change(userWordInput, { target: { value: 'longuserwordexceeding' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('For security reasons, we will remove 5 characters from your word to insert new characters and thus make it more secure.');
+  expect(userWordOutput.getAttribute('value')).toHaveLength(16);
+
+  // Set user word to a length equal to the length
+  fireEvent.change(userWordInput, { target: { value: 'sixteenletters' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('For security reasons, we will remove 5 characters from your word to insert new characters and thus make it more secure.');
+  expect(userWordOutput.getAttribute('value')).toHaveLength(16);
+});
+
+test('adjusts length if it is less than 12 or greater than 32', () => {
+  const { generateButton, lengthInput } = setup();
+
+  // Set length to an invalid value (less than 12)
+  fireEvent.change(lengthInput, { target: { value: '10' } });
+  fireEvent.click(generateButton);
+  
+  expect(message.info).toHaveBeenCalledWith('Invalid length value. Generating password with length between 12 and 32 characters.');
+  expect(Number(lengthInput.value)).toBeGreaterThanOrEqual(12);
+  expect(Number(lengthInput.value)).toBeLessThanOrEqual(32);
+
+  // Set length to an invalid value (greater than 32)
+  fireEvent.change(lengthInput, { target: { value: '33' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('Invalid length value. Generating password with length between 12 and 32 characters.');
+  expect(Number(lengthInput.value)).toBeGreaterThanOrEqual(12);
+  expect(Number(lengthInput.value)).toBeLessThanOrEqual(32);
+});
+
+test('trims user word if its length is greater than or equal to the adjusted length', () => {
+  const { generateButton, lengthInput, userWordInput } = setup();
+
+  // Set a valid length
+  fireEvent.change(lengthInput, { target: { value: '16' } });
+
+  // Set user word to a length greater than the length
+  fireEvent.change(userWordInput, { target: { value: 'longuserwordexceeding' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('For security reasons, we will remove 5 characters from your word to insert new characters and thus make it more secure.');
+  expect(userWordInput.value.length).toBe(21);
+
+  // Set user word to a length equal to the length
+  fireEvent.change(userWordInput, { target: { value: 'sixteenletters' } });
+  fireEvent.click(generateButton);
+
+  expect(message.info).toHaveBeenCalledWith('For security reasons, we will remove 5 characters from your word to insert new characters and thus make it more secure.');
+  expect(userWordInput.value.length).toBe(14);
+});
+
+test('does not allow three consecutive identical characters', () => {
+  const { generateButton, passwordInput } = setup();
+
+  fireEvent.click(generateButton);
+
+  const passwordValue = passwordInput.value;
+  const hasThreeConsecutiveChars = /(.)\1\1/.test(passwordValue);
+  expect(hasThreeConsecutiveChars).toBe(false);
 });
---
+++ src/test/src/index.test.tsx
@@ -0,0 +1,25 @@
+// index.test.tsx
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../../pages/App';
+import './assets/styles/index.css';
+
+// Mock ReactDOM.createRoot
+jest.mock('react-dom/client', () => ({
+  createRoot: () => ({
+    render: jest.fn(),
+  }),
+}));
+
+test('renders the App component without crashing', () => {
+  const rootDiv = document.createElement('div');
+  rootDiv.id = 'root';
+  document.body.appendChild(rootDiv);
+
+  const { createRoot } = require('react-dom/client');
+  const rootInstance = createRoot(rootDiv);
+
+  rootInstance.render(<App />);
+
+  expect(rootInstance.render).toHaveBeenCalledWith(<App />);
+});
